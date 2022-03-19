0a486588b9da9d90ee3569c0f89378a7a6a22f60
TaeimShin <poroon7@gmail.com>
2022-03-19 14:24:01 +0900

    14:23 STI

--- src/main/java/com/kosmo/freepproject/ReviewController.java
+++ src/main/java/com/kosmo/freepproject/ReviewController.java
@@ -71,7 +71,7 @@ public class ReviewController {
 		
 		//해당 페이지에 출력할 게시물의 구간을 계산한다. 
 		int start = (nowPage-1) * pageSize + 1;
-		int end = pageSize * nowPage;  
+		int end = 8;  
 		
 		//게시물 데이터 읽어오기
 		ArrayList<ReviewBoardDTO> lists =
@@ -116,7 +116,7 @@ public class ReviewController {
 		
 		//해당 페이지에 출력할 게시물의 구간을 계산한다. 
 		int start = (nowPage-1) * pageSize + 1;
-		int end = pageSize * nowPage;
+		int end = 8;
 
 		parameterDTO.setStart(start);
 		parameterDTO.setEnd(end);
@@ -527,7 +527,7 @@ public class ReviewController {
 		
 		//해당 페이지에 출력할 게시물의 구간을 계산한다. 
 		int start = (nowPage-1) * pageSize + 1;
-		int end = pageSize * nowPage;  
+		int end = 8;  
 		
 		//게시물 데이터 읽어오기
 		ArrayList<ReviewBoardDTO> lists =
